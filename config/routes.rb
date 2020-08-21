3a05ab682c806620cc1597556795861afd274693
Jeff Li <jeff.chun.yinli@gmail.com>
2020-08-20 22:59:19 -0400

    added route + controller action to show payroll report

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/payroll_report', to: 'payroll_report#index'
 end
