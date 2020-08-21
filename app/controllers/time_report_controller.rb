afeecfab591a53e3089cdb17b3c9e3a9a95a9873
Jeff Li <jeff.chun.yinli@gmail.com>
2020-08-21 16:58:04 -0400

    added case where the csv file has been uploaded before

--- app/controllers/time_report_controller.rb
+++ app/controllers/time_report_controller.rb
@@ -2,6 +2,12 @@ class TimeReportController < ApplicationController
   def create
     file = params[:file]
 
+    report_id = file.original_filename.split('-').last
+
+    if TimeReport.exists?(report_id: report_id)
+      render json: {error: "#{file.original_filename} has been uploaded already"} and return
+    end  
+
     unless Dir.exist?(Rails.root.to_s + "/files_storage")
       FileUtils.mkdir(Rails.root.to_s + "/files_storage")
     end
@@ -11,13 +17,12 @@ class TimeReportController < ApplicationController
     FileUtils.mv(file.tempfile.path, file_path)
     TimeReportCsvImporter.new.import(file_path)
 
-    report_id = file.original_filename.split('-').last
-
-    TimeReport.create(
+    time_report = TimeReport.create(
       upload_status: TimeReport.upload_statuses[:success],
       report_id: report_id,
       report_file_name: file.original_filename  
     )
     
+    render json: { time_report_id: time_report.id}
   end  
 end  
--- spec/controllers/time_report_controller_spec.rb
+++ spec/controllers/time_report_controller_spec.rb
@@ -15,6 +15,19 @@ RSpec.describe TimeReportController do
       expect(time_report.upload_status).to eq('success')
       expect(time_report.report_id).to eq(10)
       expect(time_report.report_file_name).to eq('time-report-10.csv')
+
+      json = JSON.parse(response.body)
+      expect(json["time_report_id"]).to eq(time_report.id)
+    end
+
+    it 'should return an error message if the file has already been uploaded' do
+      TimeReport.create(upload_status: 'success', report_id: 10, report_file_name: 'time-report-10.csv')
+      file = Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/time-report-10.csv", 'text/csv')
+
+      post :create, params: { file: file }
+
+      json = JSON.parse(response.body)
+      expect(json["error"]).to eq('time-report-10.csv has been uploaded already')
     end
   end
 end    
